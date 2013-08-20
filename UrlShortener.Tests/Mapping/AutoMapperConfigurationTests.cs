6e64f2f6ca0cb18b770ebdd8d5b012bffe83c626
David Buckingham <david.buckingham@cbeyond.net>
2013-08-20 14:23:33 -0400

    added UrlShortner.Tests project; created AutoMapperConfigurationTests

---
+++ UrlShortener.Tests/Mapping/AutoMapperConfigurationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using NUnit.Framework;
+using UrlShortener.Business.Mapping;
+
+namespace UrlShortener.Tests.Mapping
+{
+    [TestFixture]
+    public class AutoMapperConfigurationTests
+    {
+        [Test]
+        public void AutoMapperConfiguration_Configure_Succeeds()
+        {
+            // Arrange
+            AutoMapperConfiguration.Configure();
+
+            // Act
+
+            // Assert
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
